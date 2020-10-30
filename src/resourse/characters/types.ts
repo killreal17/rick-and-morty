dbd7364208f49a55d57f4393a5822c75020ccd18
killreal17 <killreal17@gmail.com>
2020-10-30 12:50:25 +0300

    feature/character-query: Удалил лишние записи персонажей, хранящихся на клиенте

--- src/resourse/characters/types.ts
+++ src/resourse/characters/types.ts
@@ -1,14 +1,4 @@
 export interface Character {
-  image: string,
   id: number,
-  name: string,
-  type: string,
-  status: 'Alive' | 'Dead' | 'unknown',
-  species: string,
-  gender: 'Male' | 'Female' | 'unknown',
-  origin: string,
-  location: string,
   episode: Set<number>,
-  url: string,
-  created: string,
 }
